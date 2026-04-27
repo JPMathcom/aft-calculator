c0ce094e18cb87c0de3c0bbfcd7046e605ba9927
Jeremiah <jpersons@seu.edu>
2026-04-27 17:10:43 -0400

    Change of range, 0-100

--- aftcalc.py
+++ aftcalc.py
@@ -2,11 +2,11 @@
 # Asked AI How I should start off a program for a caclculator and it said to start with a def function, name, and parameter.
 def get_event_score(event_name):
     print(f"\n{event_name}")
-    score = int(input("Enter score for this event, 60-100: "))
+    score = int(input("Enter score for this event, 0-100: "))
 
-    while score < 60 or score > 100:
-        print("Invalid score. Score must be between 60 and 100.")
-        score = int(input("Enter score for this event, 60-100: "))
+    while score < 0 or score > 100:
+        print("Invalid score. Score must be between 0 and 100.")
+        score = int(input("Enter score for this event, 0-100: "))
 
     return score
 
